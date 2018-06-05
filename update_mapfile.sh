ddb280e35376a3ef9313396884acc028ba283ae5
Rafael Diniz <rafael@riseup.net>
2018-06-05 18:40:21 -0300

    Added a working setup.

---
+++ update_mapfile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MAPFILE=/var/www/html/map/htdocs/itasca.map
+MAPHEADER=/var/www/html/map/htdocs/itasca.header
+MAPFOOTER=/var/www/html/map/htdocs/itasca.footer
+MAPMSGDIR=/var/www/html/map/htdocs/msg
+
+SPOOLDIR=/var/spool/outgoing_messages
+MAPDIR=/var/www/html/map/htdocs
+
+
+
+while true; do
+
+    FILENAME=$(inotifywait -e close_write -e moved_to ${SPOOLDIR} | cut -d " " -f 3)
+    
+    if echo ${FILENAME} | grep 'map'; then
+	mv ${SPOOLDIR}/${FILENAME} ${MAPMSGDIR}/${FILENAME}
+	echo "MOVED ${FILENAME}"
+    fi
+
+    cat ${MAPHEADER} > ${MAPFILE}
+    for i in $(ls -1 ${MAPMSGDIR}); do
+	cat ${MAPMSGDIR}/$i >> ${MAPFILE}
+	echo "Adding to map ${i}"
+    done
+    cat ${MAPFOOTER} >> ${MAPFILE}
+    
+done;
+
